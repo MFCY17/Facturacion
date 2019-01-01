4db203ed04da39e63d292267879aa95f0402fb7a
MFCY17 <marcelocarrion1997@gmail.com>
2019-01-01 11:16:49 -0500

    Credito

--- Sistema/Facturacion/views.py
+++ Sistema/Facturacion/views.py
@@ -4,38 +4,68 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+# Templete Login
 def login (request):
 	return render(request,"Facturacion/login/login.html",{})
-
 def restablecer (request):
     return render(request,"Facturacion/login/restablecer_login.html",{})
 
+
+# Template base 
 def base (request): 
-    return render(request,"Facturacion/base.html",{})  
+    return render(request,"Facturacion/base.html",{}) 
 
+
+#Templete de Clientes
 def clientes (request): 
-    return render(request,"Facturacion/administracion/crear_clientes.html",{})  
-
+    return render(request,"Facturacion/administracion/Clientes/crear_clientes.html",{})  
 def clientes_list (request): 
-    return render(request,"Facturacion/administracion/listar_clientes.html",{}) 
+    return render(request,"Facturacion/administracion/Clientes/listar_clientes.html",{}) 
 
+
+#Templete de Productos
 def productos (request): 
-    return render(request,"Facturacion/administracion/crear_productos.html",{})
-
+    return render(request,"Facturacion/administracion/Productos/crear_productos.html",{})
 def productos_list (request): 
-    return render(request,"Facturacion/administracion/listar_productos.html",{})
-    
+    return render(request,"Facturacion/administracion/Productos/listar_productos.html",{})
+
+
+#Templete de Administracion de Accesos/Usuarios   
 def usuarios (request):
-    return render(request,"Facturacion/administracion/usuarios.html", {})     
+    return render(request,"Facturacion/administracion/Usuarios/usuarios.html", {})
+def usuarios_list (request):
+    return render(request,"Facturacion/administracion/Usuarios/listar_usuarios.html", {})
 
+
+#Templete de Facturacion(Factura)   
 def factura_venta (request): 
-    return render(request,"Facturacion/administracion/factura_venta.html",{})  
-
+    return render(request,"Facturacion/administracion/Factura/factura_venta.html",{})  
 def factura_list (request): 
-    return render(request,"Facturacion/administracion/listar_factura.html",{})
+    return render(request,"Facturacion/administracion/Factura/listar_factura.html",{})
 
+
+#Templete de Creditos
+def credito_list (request): 
+    return render(request,"Facturacion/administracion/Creditos/listar_credito.html",{})
+
+
+#Templete de Reportes
+def reporte_venta (request): 
+    return render(request,"Facturacion/administracion/Reportes/reportes_ventas.html",{})
+
+
+#Templete de Configuración
+def perfil_empresa (request): 
+    return render(request,"Facturacion/administracion/Configuracion/perfil_de_la_empresa.html",{})
+def perfil_usuario (request): 
+    return render(request,"Facturacion/administracion/Configuracion/perfil_de_usuario.html",{})
+def configuracion (request): 
+    return render(request,"Facturacion/administracion/Configuracion/configuracion.html",{})
+
+
+#Templete de Herramientas
 def calendario (request): 
-    return render(request,"Facturacion/administracion/calendario.html",{})
-
+    return render(request,"Facturacion/administracion/Herramientas/calendario.html",{})
 def calculadora (request): 
-    return render(request,"Facturacion/administracion/calculadora.html",{})   
+    return render(request,"Facturacion/administracion/Herramientas/calculadora.html",{})   
--- Sistema/Sistema/urls.py
+++ Sistema/Sistema/urls.py
@@ -19,16 +19,41 @@ from Facturacion import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    #Login
     url(r'^$', views.login, name='login'),
     url(r'^restablecer/', views.restablecer, name='restablecer'),
+
+    #Base 
     url(r'^administracion/', views.base, name='base'),
+
+    #Clientes
     url(r'^crear_clientes/', views.clientes, name='clientes'),
     url(r'^listar_clientes/', views.clientes_list, name='clientes_list'),
+
+    #Productos
     url(r'^crear_productos/', views.productos, name='productos'),
     url(r'^listar_productos/', views.productos_list, name='productos_list'),
+
+    #Administracion de accesos(Usuarios)
     url(r'^crear_usuarios/', views.usuarios, name='usuarios'),
+    url(r'^listar_usuarios/', views.usuarios_list, name='usuarios_list'),
+
+    #Facturacion
     url(r'^nueva_venta/', views.factura_venta, name='factura_venta'),
     url(r'^listar_factura/', views.factura_list, name='factura_list'),
+
+    #Creditos
+    url(r'^listar_credito/', views.credito_list, name='credito_list'),
+
+    #Reportes
+    url(r'^reportes_ventas/', views.reporte_venta, name='reporte_venta'),
+
+    #Configuracion
+    url(r'^perfil_empresa/', views.perfil_empresa, name='perfil_empresa'),
+    url(r'^perfil_usuario/', views.perfil_usuario, name='perfil_usuario'),
+    url(r'^configuracion/', views.configuracion, name='configuracion'),
+
+    #Herramientas
     url(r'^calendario/', views.calendario, name='calendario'),
     url(r'^calculadora/', views.calculadora, name='calculadora'),
 
